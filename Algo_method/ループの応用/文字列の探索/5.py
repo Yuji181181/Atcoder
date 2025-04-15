1661a6ad93188a60590d17887518b64fc384edc7
Yuji181181 <s1f102402552@iniad.org>
2025-04-15 17:58:15 +0900

    5 間違い

---
+++ Algo_method/ループの応用/文字列の探索/5.py
@@ -0,0 +1,12 @@
+N = int(input())
+S = input()
+T = input()
+count = 0
+
+for i in range(N):
+    if S[i] == T[i]:
+        count += 1
+    if S[i] or T[i] == "?":
+        count += 1
+
+print(count)
