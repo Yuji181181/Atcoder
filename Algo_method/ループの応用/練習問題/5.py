8182fc05a8a8572414ff0f50259eb55e25dc98f8
Yuji181181 <s1f102402552@iniad.org>
2025-04-16 13:25:03 +0900

    5

--- Algo_method/ループの応用/練習問題/5.py
+++ Algo_method/ループの応用/練習問題/5.py
@@ -2,4 +2,14 @@ N = int(input())
 S = input()
 result = ""
 
-for i in 
+for i in S:
+    if i == "A":
+        result += "T"
+    elif i == "T":
+        result += "A"
+    elif i == "G":
+        result += "C"
+    elif i == "C":
+        result += "G"
+
+print(result)
