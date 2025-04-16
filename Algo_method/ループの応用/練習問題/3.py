c9995a5031196d1f025c5dcac7b5dd792b3d5463
Yuji181181 <s1f102402552@iniad.org>
2025-04-16 11:47:48 +0900

    3 not in 修正

--- Algo_method/ループの応用/練習問題/3.py
+++ Algo_method/ループの応用/練習問題/3.py
@@ -1,6 +1,5 @@
 N = int(input())
 
 for i in range(1, N + 1):
-    if "4" in str(i) or "9" in str(i):
-        continue  # 4,9が入ってるとき、print(x)を無視してfor文冒頭に戻る
-    print(i)
+    if "4" not in str(i) and "9" not in str(i):
+        print(i)
