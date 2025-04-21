6e8187f6abd045bfabefe8ab14fe8bc2277f5963
Yuji181181 <s1f102402552@iniad.org>
2025-04-22 06:54:08 +0900

    FizzBuzz 終

--- Algo_method/FizzBuzz/if文と集合/4.py
+++ Algo_method/FizzBuzz/if文と集合/4.py
@@ -1,11 +1,11 @@
 N = int(input())
 
 for i in range(1, N + 1):
-    if N % 15 == 0:
+    if i % 15 == 0:
         print("FizzBuzz")
-    elif N % 5 == 0:
+    elif i % 5 == 0:
         print("Buzz")
-    elif N % 3 == 0:
+    elif i % 3 == 0:
         print("Fizz")
     else:
-        print(N)
+        print(i)
--- Algo_method/FizzBuzz/if文と集合/5.py
+++ Algo_method/FizzBuzz/if文と集合/5.py
@@ -2,7 +2,7 @@ N = int(input())
 count = 0
 
 for i in range(1, N + 1):
-    if N % 3 == 0 and N % 5 != 0:
+    if i % 3 == 0 and i % 5 != 0:
         count += 1
 
 print(count)
