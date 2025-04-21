077e54a3571866303796fc447b1ca9e9861687fc
Yuji181181 <s1f102402552@iniad.org>
2025-04-22 05:42:30 +0900

    6

---
+++ Algo_method/FizzBuzz/割り算/6.py
@@ -0,0 +1,16 @@
+D = int(input())
+
+if D % 7 == 0:
+    print("Thu")
+if D % 7 == 1:
+    print("Fri")
+if D % 7 == 2:
+    print("Sat")
+if D % 7 == 3:
+    print("Sun")
+if D % 7 == 4:
+    print("Mon")
+if D % 7 == 5:
+    print("Tue")
+if D % 7 == 6:
+    print("Wed")
