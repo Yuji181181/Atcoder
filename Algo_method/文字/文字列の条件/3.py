d26523dfe5f7380d0761e064cb4959b35aec69f4
Yuji181181 <s1f102402552@iniad.org>
2025-04-23 14:04:57 +0900

    3

---
+++ Algo_method/文字/文字列の条件/3.py
@@ -0,0 +1,15 @@
+S = input()
+
+
+def password(S):
+    # 連続するか判定
+    for i in range(3):
+        if int(S[i + 1]) - int(S[i]) != 1:
+            return False
+    return True
+
+
+if password(S) == True:
+    print("dangerous")
+else:
+    print("safe")
