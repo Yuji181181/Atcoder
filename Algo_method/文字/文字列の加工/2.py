24692734abecfb1a191f3683f9f4ecc6de2f1627
Yuji181181 <s1f102402552@iniad.org>
2025-05-06 03:44:36 +0900

    2

--- Algo_method/文字/文字列の加工/2.py
+++ Algo_method/文字/文字列の加工/2.py
@@ -1,17 +1,16 @@
-M = int(input())
+M = input()
 count = 0
 
 for i in range(9):
     if i % 2 == 0:
-        count += int(N[i])
+        count += int(M[i])
     if i % 2 == 1:
-        count += int(N[i]) * 2
+        count += int(M[i]) * 2
 
 
 plus = count % 10
-answer = int(N + str(plus))
 
-if M == answer:
+if int(M[9]) == plus:
     print("Yes")
 else:
     print("No")
